bf1f7ea8d3a9028bd7441c084133e39b0b810a7a
Robin Malhotra <me@rmalhotra.com>
2018-07-30 19:15:50 +0530

    Initial Commit

---
+++ ObserverTesting/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  ObserverTesting
+//
+//  Created by Robin Malhotra on 30/07/18.
+//  Copyright © 2018 Robin Malhotra. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        configureView()
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
